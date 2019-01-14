a8ed28757f462ddf5b40314af3f1a0ba9bf6a2a5
oooolivierrrr <43121341+oooolivierrrr@users.noreply.github.com>
2019-01-14 04:45:52 -0500

    corrected error

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-const App = () => {
-  <div className="container">App Component</div>;
-};
+const App = () => <div className="container">App Component</div>;
 
 export default App;
